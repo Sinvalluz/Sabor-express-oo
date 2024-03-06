e66499268ccd6f87e399c616f92aa1b033732193
Sinval Luz <contato.sinvalluz@gmail.com>
2024-03-06 15:26:56 -0300

    Adicionando @property e @clasmethod ao projeto

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,10 +1,29 @@
 class Restaurante():
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'praca'
-restaurante_praca.categoria = 'Goumert'
+    def __str__(self):
+        return f'{self._nome} | {self._categoria}'
 
-print(vars(restaurante_praca))
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐' 
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo           
+    
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
+
+Restaurante.listar_restaurantes()
