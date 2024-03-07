02c4b201128d0b635cc5417b7c16cf28fa070ec4
Sinval Luz <contato.sinvalluz@gmail.com>
2024-03-07 16:57:45 -0300

    Importando funções e finalizando projeto

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,9 +1,12 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante():
     restaurantes = []
     def __init__(self, nome, categoria):
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -11,19 +14,27 @@ class Restaurante():
 
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐' 
 
     def alternar_estado(self):
-        self._ativo = not self._ativo           
-    
-restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.alternar_estado()
-restaurante_pizza = Restaurante('pizza express', 'Italiana')
+        self._ativo = not self._ativo
 
-Restaurante.listar_restaurantes()
+    def receber_avaliacao(self, cliente, nota):
+        if 0 < nota < 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return '-'
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1 )
+        return media
